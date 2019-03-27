4da65a34d824af08ccc205fcb9396593ad1f571e
Dario <dariogabriel113@gmail.com>
2019-03-27 16:30:26 -0300

    Inicializa componentes

--- app/src/main/java/com/dariogabriellsilva/navigationdrawerdemo/MainActivity.java
+++ app/src/main/java/com/dariogabriellsilva/navigationdrawerdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dariogabriellsilva.navigationdrawerdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
     }
 }
